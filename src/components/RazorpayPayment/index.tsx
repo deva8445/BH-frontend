9f668562dc4b77caab43a5ca3438f8dea056f899
Devashish-jft <devashish.kumar@jellyfishtechnologies.com>
2024-07-30 15:51:50 +0530

    rozerpay integration in ui

---
+++ src/components/RazorpayPayment/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RazorPay = () => {
+  return <div>index</div>;
+};
--- src/components/cartBox/index.tsx
+++ src/components/cartBox/index.tsx
@@ -1,19 +1,68 @@
 import React, { FC, useState } from "react";
 import { TruncateString } from "../../utils/helper";
-import Auth from "../auth";
-import CustomModal from "../modal";
-import { CustomModalStyle } from "../modal/style";
 import { AuthUser } from "../../services/auth-user";
 import { ROLE } from "../../constants/enum";
+import axios from "axios";
 interface ICartBook {
   data: any;
   handleAddToCart?: any;
+}
+
+declare global {
+  interface Window {
+    Razorpay: any;
+  }
 }
 
 const CartBox: FC<ICartBook> = ({ data, handleAddToCart }) => {
   const { getUser } = AuthUser();
   const user = getUser();
   const userType = user?.userType;
+
+  const handlePayment = async () => {
+    try {
+      const response = await axios.post("/payment/order", {
+        amount: data.price,
+        currency: "INR",
+        receipt: `receipt_${data.id}`,
+      });
+      const { id, amount, currency } = response.data.data;
+      const options = {
+        key: process.env.REACT_APP_RAZORPAY_KEY_ID,
+        amount: amount?.toString(),
+        currency: currency,
+        name: "Deva Treaders & Co.",
+        description: "Shop Transection",
+        order_id: id,
+        // handler: async (response: any) => {
+        //   const paymentData = {
+        //     orderId: response?.razorpay_order_id,
+        //     paymentId: response.razorpay_payment_id,
+        //     signature: response?.razorpay_signature,
+        //   };
+
+        //   await axios.post("/payment/verify", paymentData);
+        // },
+        prefill: {
+          name: user?.name,
+          email: user?.email,
+          contact: "7008551963",
+        },
+        notes: {
+          address: "Deva Treaders & Co.",
+        },
+        theme: {
+          color: "#F37254",
+        },
+      };
+
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+    } catch (error) {
+      console.error("Payment error:", error);
+      console.log("Payment failed. Please try again.");
+    }
+  };
   return (
     <div className="border shadow-xl w-[220px] rounded-lg overflow-hidden cursor-pointer">
       <div className="flex justify-center">
@@ -48,7 +97,7 @@ const CartBox: FC<ICartBook> = ({ data, handleAddToCart }) => {
           <div className=" py-4">
             <div
               className="border p-2 w-full text-center bg-blue-500 rounded-md text-white cursor-pointer hover:text-orange-600 hover:bg-blue-300"
-              onClick={handleAddToCart}
+              onClick={handlePayment}
             >
               Add to cart
             </div>
